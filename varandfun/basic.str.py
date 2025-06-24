1e9b6a5450c16f18c637f478a8b1b5d6d4af1363
mrxpeacey <mrxpeacey@gmail.com>
2025-06-24 17:59:17 +0545

    basic string

---
+++ varandfun/basic.str.py
@@ -0,0 +1,13 @@
+#Asking input to user
+x=input("Enter your name:");
+#Printing the output
+print("hello",x,end=" ",sep="  ");#bydefault the print function takes to the new line
+print("Is already in the new line");
+print(type("hello"));
+print(x.strip().upper());
+#
+a,b=5, 6;
+print(a,b);
+#
+first,second=x.split();#splits into two part
+print("hello",first);
