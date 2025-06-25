5f024ca4c06b59dae7992f69f39f65a034c0fbae
mrxpeacey <mrxpeacey@gmail.com>
2025-06-25 19:51:41 +0545

    loops

---
+++ loop/loops.py
@@ -0,0 +1,16 @@
+print("hello"*3);
+i=0;
+while(i<3):
+    print("MEow");
+    i+=1;
+    break;
+
+name=["ram","sham","hari"];
+for i in name:
+    print(i);
+hell=["hello"];
+for i in hell:
+    print(i);
+for i in "1234":
+    print("hi");
+print(len(name));#len of list=number of items in the list
