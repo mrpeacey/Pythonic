77948504bc7272f41ed0db14aa63b130e7b99f61
mrxpeacey <mrxpeacey@gmail.com>
2025-06-25 20:55:11 +0545

    loop

---
+++ loop/simple.pattern.py
@@ -0,0 +1,11 @@
+for i in range(3):#just a simple square
+    for j in range(4):
+    
+        print("*",end="");
+    print();
+def asterisk():
+    for i in range(5):
+        for j in range(i):
+            print("*",end="");
+        print();
+asterisk();
